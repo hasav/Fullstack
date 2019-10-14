ed69dd636123c7339924ef08f7354da75f735087
hasav <vlj.hasa@gmail.com>
2019-10-14 19:07:45 +0300

    5.2 done, added useEffect for fetching user from local storage, logout button added

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -22,7 +22,7 @@ const  App = () => {
       })
 
       window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
+        'loggedBlogappUser', JSON.stringify(user)
       )
       blogService.setToken(user.token)
       setUser(user)
@@ -91,6 +91,22 @@ const  App = () => {
     )
   }
 
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  useEffect(() => {
+    
+    const loggedUserJSON =  window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
   useEffect(() => {
     blogService
       .getAll()
@@ -115,7 +131,7 @@ const  App = () => {
     <div>
       <Notification message={message} />
       <h2>blogs</h2>
-      <p>{user.name} logged in </p>
+      <p>{user.name} logged in <button onClick={logout}>Logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
