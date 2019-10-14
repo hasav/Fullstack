24bcf2698bf61391d222a60b4b5ad3fbc8faea2a
hasav <vlj.hasa@gmail.com>
2019-10-14 18:46:03 +0300

    5.1 done, login works with token validation, blo

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import loginService from './services/login'
+import blogService from './services/blogs'
+
+
+
+const  App = () => {
+
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
+  const [color, setColor] = useState('green')
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setColor('green')
+      setMessage(`Login successful!`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+      
+    } catch (exception) {
+      setColor('red')
+      setMessage('wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+
+  }
+
+  
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const Notification = ({ message }) => {
+
+    var style = {
+      color: color,
+      background: 'lightgrey',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }
+
+    if (message === null || message === '') {
+      return null
+    }
+    return (
+      <div style={style}>
+      {message}
+    </div>
+    )
+  }
+
+  useEffect(() => {
+    blogService
+      .getAll()
+      .then(initialBlogs => {
+        setBlogs(initialBlogs)
+      })
+  }, [])
+
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification message={message} />
+          {loginForm()}
+
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Notification message={message} />
+      <h2>blogs</h2>
+      <p>{user.name} logged in </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+export default App;
