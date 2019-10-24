b3e728c0d887024b139d2ee26431b63363d9b1cb
hasav <vlj.hasa@gmail.com>
2019-10-24 10:57:21 +0300

    5.6 done, more info about blogs can be seen if div is pressed, need to fix bug of disappearing blog when like-button is pressed

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [moreInfo, setMoreInfo] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const returnInfo = () => {
+    if (moreInfo) {
+      return (
+      <div>
+          <p>{blog.title}</p>
+          <a href={blog.url}>{blog.url}</a>
+          <p>{blog.likes} likes
+          <button onClick={console.log("click")}>like</button>
+          </p>
+          <p>added by {blog.author}</p>
+
+      </div>
+      )
+    }
+    return (
+      <div>
+        {blog.title} {blog.author}
+      </div>
+    )
+  }
+
+  const toggleInfo = () => {
+    setMoreInfo(!moreInfo)
+  }
+  return (
+    <div style={blogStyle}> 
+      <div onClick={toggleInfo}>
+        {returnInfo()}
+      </div>
   </div>
-)
+)}
 
 export default Blog
