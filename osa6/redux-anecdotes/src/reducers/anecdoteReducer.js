5ef18826b4e6b2d3f78879412cb06ef04a97a751
hasav <vlj.hasa@gmail.com>
2019-12-09 18:40:17 +0200

    added json server as a backend and set initial anecdotes to be fetched from there

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,9 +10,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch(action.type) {
     case 'VOTE':
       const id = action.data.id
@@ -35,6 +26,8 @@ const anecdoteReducer = (state = initialState, action) => {
     case 'NEW_ANECDOTE':
       return [...state, action.data]
       
+    case 'INIT_ANECDOTES':
+      return action.data
     default:
       return state
   }
@@ -55,6 +48,13 @@ export const createAnecdote = (content) => {
       votes: 0,
       id: getId()
     }
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
   }
 }
 
